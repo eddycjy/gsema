e4b308ad7ce06800c82ca4e5d29db49f1778aecc
EDDYCJY <313687982@qq.com>
2019-01-19 14:41:26 +0800

    add a simple goroutine limit pool.

---
+++ sema.go
@@ -0,0 +1,29 @@
+package gsema
+
+import "sync"
+
+type Semaphore struct {
+	c  chan struct{}
+	wg *sync.WaitGroup
+}
+
+func NewSemaphore(maxSize int) *Semaphore {
+	return &Semaphore{
+		c:  make(chan struct{}, maxSize),
+		wg: new(sync.WaitGroup),
+	}
+}
+
+func (s *Semaphore) Add(delta int) {
+	s.wg.Add(delta)
+	s.c <- struct{}{}
+}
+
+func (s *Semaphore) Done() {
+	<-s.c
+	s.wg.Done()
+}
+
+func (s *Semaphore) Wait() {
+	s.wg.Wait()
+}
